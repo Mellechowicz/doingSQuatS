7fcb2cadd791dbd8a7c2f2b3e427e1ceecb3fe76
Andrzej P. Kądzielawa <apkadzielawa@gmail.com>
2022-12-27 17:01:46 +0100

    A workflow example

--- examples/workflow.sh
+++ examples/workflow.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 OLD=./
-NEW=Fe38Pt16
+NEW=Fe46Pt08
 
 for POSCAR in $(awk '{print $1}' ${OLD}/cell.in); do
   python3 ../EXPAND_CELL.py --help
-  python3 ../EXPAND_CELL.py ${OLD}/${POSCAR} 16 Pt Fe 128000 _${NEW}
-done                                         || exit
+  echo python3 ../EXPAND_CELL.py ${OLD}/${POSCAR} 8 Pt Fe 12800 _${NEW}
+  python3      ../EXPAND_CELL.py ${OLD}/${POSCAR} 8 Pt Fe 12800 _${NEW}
+done                                                      || exit
 
-../_symmetry.py _${NEW} ${NEW} delete        || exit
-rm -r _${NEW}                                || exit
+python3 ../_symmetry.py --help
+echo python3 ../_symmetry.py _${NEW} ${NEW} delete        || exit
+python3      ../_symmetry.py _${NEW} ${NEW} delete        || exit
+rm -r _${NEW}                                             || exit
+if [ -s different_directory.txt ]; then
+    NEW=$(cat different_directory.txt)
+    rm different_directory.txt
+fi
 
-cd ${NEW}                                    || exit
-../../_combine_stats.sh                      || exit 
-../../_unique.sh                             || exit 
-rm -r [0-9]*                                 || exit 
-mv unique/* ./                               || exit 
-rm -r unique                                 || exit 
-../../_correlation_function.py ./ radial yes || exit 
+cd ${NEW}                                                 || exit
+echo sh ../../_combine_stats.sh                           || exit 
+sh ../../_combine_stats.sh                                || exit 
+echo sh ../../_unique.sh                                  || exit 
+sh ../../_unique.sh                                       || exit 
+rm -r [0-9]*                                              || exit 
+mv unique/* ./                                            || exit 
+rm -r unique                                              || exit 
+echo python3 ../../_correlation_function.py ./ radial yes || exit 
+python3      ../../_correlation_function.py ./ radial yes || exit 
